3a760c3adfe2c617d45722617d4a1e0b3f51c4e4
dhanesh03 <dhanesh.prajapati@risklatte.xyz>
2020-08-14 00:42:26 +0530

    update till 2 unique value

--- tests/temp.py
+++ tests/temp.py
@@ -1,9 +1,18 @@
 import pandas as pd
 import re
 
-class test_train_split():
+class data_filter():
     def __init__(self, df):
+        """ Identify the index column
 
+        creating a temp variable to store the index column
+
+        temp
+        -------------
+        if there is an index the len will be more than 0
+        if the index column exist will be considered while importing
+
+        """
         temp = re.findall(r"(?i)un" or r"(?i)sr" or r"(?i)ind", df.columns[0])
         
         if len(temp) > 0:
@@ -26,7 +35,26 @@ class test_train_split():
         cat_dict: summary df with col index as key which needs to be change
         """
         col_type = self.df.dtypes
+        col_name = list(self.df)
         cat_var_index = [i for i, x in enumerate(col_type) if x == 'object']
+
+        for i in cat_var_index:
+
+            unique_col = pd.unique(self.df[self.df.columns[i]])
+
+            if len(unique_col) == 2:
+                dict_unique_2 = {}
+
+                for j, k in enumerate(unique_col):
+                    dict_unique_2[k] = j
+                self.is_categorical[i] = dict_unique_2
+
+            elif len(unique_col) == 1:
+                self.is_categorical[i] = 1
+
+            # else:
+
+
 
         return cat_var_index
 
@@ -36,8 +64,11 @@ class test_train_split():
 
 
 df = pd.read_excel("onehot.xlsx")
-a = test_train_split(df)
-print(a.categorica_var())
+a = data_filter(df)
+a_1= a.categorica_var()
+print(a.df.columns[a_1[0]])
+# print(list(pd.get_dummies(a.df[a.df.columns[a_1[1]]])))
+# print(pd.get_dummies(a1))
 # x = re.findall(r"(?i)unnamed" or r"(?i)sr" or r"(?i)index", df.columns[0])
 # print(len(x))
 # print(len(x)>0)
